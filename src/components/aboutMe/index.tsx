6828bec7d76d2cf3066ad454a5ca71ff63015b1a
Gourav Manna <gmanna@intralinks.com>
2021-08-25 00:02:53 +0530

    feat: added about me section

---
+++ src/components/aboutMe/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import gouravImage from '../../assets/images/gourav.jpg'
+
+import styles from './aboutMe.module.scss';
+
+const AboutMe = () => {
+  return (
+    <div className={styles.container}>
+      <FontAwesomeIcon icon={faUser} size="2x"/>
+      <p className={styles.aboutMeText}><span>About me</span></p>
+      <section className={styles.subContainer}>
+        <article className={styles.imageInfo}>
+          <img src={gouravImage} alt="gouravImage" />
+          <figcaption>Gourav Manna</figcaption>
+        </article>
+        <article className={styles.aboutMeParah}>
+
+        </article>
+      </section>
+      
+    </div>
+  )
+}
+
+export default AboutMe;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Intro from './Intro';
 
 import './index.css';
+import AboutMe from './aboutMe';
 
 const MainContainer = () => {
   return (
-    <div className="container" >
-      <nav className="navBar">
-        <h1 className="myName">GOURAV MANNA</h1>
-      </nav>
-      <Intro />
+    <div>
+      <div className="container" >
+        <nav className="navBar">
+          <h1 className="myName">GOURAV MANNA</h1>
+        </nav>
+        <Intro />
+      </div>
+     <AboutMe />
     </div>
   )
 }
