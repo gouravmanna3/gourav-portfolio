62fe5ac09569f88d35dd18b86a89b48c29daeaaf
Gourav Manna <gmanna@intralinks.com>
2021-09-10 23:29:34 +0530

    feat: added educatiion section

--- src/components/educationExperience/index.tsx
+++ src/components/educationExperience/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faBookReader, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faBookReader, faLaptopCode, faUserGraduate, faSchool, faUniversity } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './educationExperience.module.scss';
@@ -12,18 +12,50 @@ const EducationExperience = () => {
           <FontAwesomeIcon icon={faBookReader} className={styles.readingIcon} />
           <p className={styles.myEducationText}>My Education</p>
         </div>
-        <div className={styles.educationList}>
-          <div>
-            <h4>B.Tech IT</h4>
-            <p>Sreenidhi Institute of Science and Technology</p>
+        <div className={styles.timeline}>
+          <div className={styles.education}>
+            <div className={styles.educationYear}>
+              <FontAwesomeIcon icon={faUserGraduate} className={styles.icon} />
+              <div>2015-2019</div>
+            </div>
+            <div className={styles.content}>
+              <div className={styles.title}>
+                B.Tech IT
+              </div>
+              <div className={styles.school}>
+                <p>Sreenidhi Institute of Science and Technology</p>
+              </div>
+            </div>
           </div>
-          <div>
-            <h4>Intermediate</h4>
-            <p>Narayana Junior College</p>
+
+          <div className={styles.education}>
+            <div className={styles.educationYear}>
+              <FontAwesomeIcon icon={faUniversity} className={styles.icon} />
+              <div>2013-2015</div>
+            </div>
+            <div className={styles.content}>
+              <div className={styles.title}>
+                Intermediate
+              </div>
+              <div className={styles.school}>
+              <p>Narayana Junior College</p>
+              </div>
+            </div>
           </div>
-          <div>
-            <h4>High School</h4>
-            <p>St Martin's High School</p>
+
+          <div className={styles.education}>
+            <div className={styles.educationYear}>
+              <FontAwesomeIcon icon={faSchool} className={styles.icon} />
+              <div>2013</div>
+            </div>
+            <div className={styles.content}>
+              <div className={styles.title}>
+                High School
+              </div>
+              <div className={styles.school}>
+              <p>St Martin's High School</p>
+              </div>
+            </div>
           </div>
         </div>
       </article>
